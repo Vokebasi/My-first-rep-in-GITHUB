583ff0f4489f34af0332aafb03a5bd7b1f7e2ecc
Dasmeliox <ismail.isabekov@mail.ru>
2022-05-17 19:21:52 +0600

    added three files

---
+++ backend.py
@@ -0,0 +1,37 @@
+# 17.05.2022
+"""
+Git (system) / Git (service)
+
+git - система контроля версий для отслеживания изменений в вашем коде, она имеет древовидную структуру
+git - is free open source version comtrol system, for tracking changes in source code, has brain structure
+
+
+github - web based hosting service for version control.
+"""
+
+# Repository - container with your code
+# Commit - это сохранка кода / is a screenshot of state, we can turnback to them whenever we want
+
+
+# main branch -- initial state
+# ____ ____ ____ ____ ____
+#                        _____ ______ _____
+
+
+# pull request - asking someone to merge the branches
+# merge - adding two branches
+
+
+
+# git --version --> showss version of your git
+# git --help --> help with git commands
+# git init --> initializes a git repository
+# git status --> shows current state of our project
+
+
+# git add <filename> or .  --> adds changes to git
+
+
+
+
+
